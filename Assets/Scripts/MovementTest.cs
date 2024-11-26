10f137472a1f3d4ebb7d69c68b48491e266ab300
Kareemrw <166079338+Kareemrw@users.noreply.github.com>
2024-11-25 22:15:12 -0800

    Dialogue Tests

--- Assets/Scripts/MovementTest.cs
+++ Assets/Scripts/MovementTest.cs
@@ -1,11 +1,17 @@
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MovementTest : MonoBehaviour
 {
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
 
-    float horizontal;
-    float vertical;
+    private bool canTalk = false;
+    private Dialogue currentDialogue;
+    private float horizontal;
+    private float vertical;
 
     public float runSpeed = 20.0f;
 
@@ -16,13 +22,62 @@ public class MovementTest : MonoBehaviour
 
     void Update()
     {
+        // Handle dialogue activation
+        if (canTalk && Input.GetKeyDown(KeyCode.E))
+        {
+            ActivateEKey(false);
+            if (currentDialogue != null && !currentDialogue.IsDialogueActive())
+            {
+                currentDialogue.StartDialogue();
+            }
+        }
+
+        // Movement input
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        // Stop movement if dialogue is active
+        if (currentDialogue != null && currentDialogue.IsDialogueActive())
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        }
+    }
 
+    private void ActivateEKey(bool isActive, Transform npcTransform = null)
+    {
+        npcTransform = npcTransform ?? currentDialogue?.transform;
+        Transform eKeyTransform = npcTransform?.Find("eKey");
+
+        if (eKeyTransform != null)
+        {
+            eKeyTransform.gameObject.SetActive(isActive);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("NPC"))
+        {
+            ActivateEKey(true, collision.transform);
+            canTalk = true;
+            currentDialogue = collision.GetComponent<Dialogue>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("NPC"))
+        {
+            ActivateEKey(false, collision.transform);
+            canTalk = false;
+            currentDialogue = null; // Clear reference
+        }
     }
 }
