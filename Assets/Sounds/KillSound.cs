a7a590e48b9d8d324c3247f45e468d10f5be91eb
Monika Ortiz <monika.ortiz.aof@gmail.com>
2024-12-05 22:02:27 -0800

    Random Changes

---
+++ Assets/Sounds/KillSound.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class KillSound : MonoBehaviour
+{
+    private void Update()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        if(currentScene.name == "Scene2toLS")
+        {
+            StartCoroutine(Pause());
+        }
+    }
+
+    IEnumerator Pause()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+
+    }
+}
